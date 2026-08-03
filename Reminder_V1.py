1e630195c4bd5f40da975c35d7b93b72f2dea00c
Parzivalmyers <ROGqiangsheng7plus@outlook.com>
2026-08-04 10:42:59 +1200

    8.4 Vision

--- Reminder_V1.py
+++ Reminder_V1.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 #Create the Main Window
 window = tk.Tk()
 window.title("Medication Reminder")
@@ -15,12 +16,30 @@ label = tk.Label(
 )
 label.pack(pady=30)
 
-def test_alert():
-    messagebox.showinfo("It's time to take your medicine!")
+#Medicine Name Input
+tk.Label(window, text= "Medicine Name: ").pack()
+med_name = tk.Entry(window, width=30)
+med_name.pack()
 
-btn_test = tk.Button(window, text="Reminder", command=test_alert)
-btn_test.pack()
+#Medication Time Input 
+tk.Label(window, text="Administration Time(HH:MM): ").pack()
+med_time = tk.Entry(window, width=30)
+med_time.pack()
 
+#Store Medicine List
+medicine_list = []
+
+def add_medicine():
+    name = med_name.get()
+    time_str = med_time.get()
+    medicine_list.append({"name":name,"time":time_str})
+    messagebox.showinfo("Successful",f"Added:{name} {time_str}")
+    med_name.delete(0,tk.END)
+    med_time.delete(0,tk.END)
+
+#Add Medicine Button    
+tk.Button(window, text="Add Medicine", command=add_medicine).pack(pady=10)
+    
 
 #Message loop(Place it at the end to keep the window displayed)
 window.mainloop()
